7ffe1542684bfe94a07760a9b5b7eeb360e82f4d
Banana-In-Black <banana.in.black@gmail.com>
2013-07-20 23:49:05 +0800

    Add event callbacks to custimize.

--- item-selector.js
+++ item-selector.js
@@ -30,11 +30,11 @@ ItemView = Backbone.View.extend({
         return this;
     },
     events: {
-        'click': 'selectItem'
+        'click': 'select'
     },
-    selectItem: function() {
+    select: function() {
         this.$el.toggleClass('pressed');
-        console.log(JSON.stringify(this.model.toJSON(), null, 4));
+        this.trigger('select', this.model);
     },
     isModelValid: function() {
         return this.model instanceof Item;
@@ -48,9 +48,12 @@ Items = Backbone.Collection.extend({
 /**
  * A View represents item collection.
  * 
- * Override callbacks to custimize behavior:
- * - confirmSelection: Button 'OK'
- * - cancelSelection: Button 'Cancel'
+ * options for new instance
+ * - validate: validation before calling confirmSelection callback, recieving an array of selected model objects (Item), 'this' refer to this View
+ * - ok: callback of Button 'OK', recieving an array of selected models, 'this' refer to this View
+ * - cancel: callback of Button 'Cancel'
+ * - cols: the number of columns to display
+ * - rows: the number of rows to display
  */
 ItemsView = Backbone.View.extend({
     cols: 4,
@@ -73,13 +76,16 @@ ItemsView = Backbone.View.extend({
         '</div>',        
     initialize: function() {
         this.collection = new Items(this.collection);
+        this.cols = this.options.cols || this.cols;
+        this.rows = this.options.rows || this.rows;
     },
     render: function() {
         if(this.isCollectionValid()) {
             var _this = this.clearView();
             var $itemsBox = _this.$el.find('.is-items-box');
             this.collection.each(function(model, index) {
-                new ItemView({ model: model }).render().appendTo($itemsBox);
+                var itemView = new ItemView({ model: model }).render().appendTo($itemsBox);
+                _this.listenTo(itemView, 'select', _this.clearMessage);
 
                 if((index + 1) % _this.cols == 0) {
                     $itemsBox.append('<div class="is-clear-both"/>');
@@ -110,11 +116,19 @@ ItemsView = Backbone.View.extend({
         this.$el.html(this.template);
         return this;
     },
-    events: {
-        'click .is-btn-cancel': 'cancelSelection', // not implement yet
-        'click .is-btn-ok': 'confirmSelection'
+    doCallback: function(func, args) {
+        if($.isFunction(func)) {
+            return func.apply(this, args);
+        } else {
+            console.log('Not a function!');
+            return null;
+        }
     },
-    confirmSelection: function() {
+    events: {
+        'click .is-btn-cancel': 'cancel', // not implement yet
+        'click .is-btn-ok': 'ok'
+    },
+    ok: function() {
         var _this = this;
         var selectedModels = new Array();
         
@@ -123,6 +137,50 @@ ItemsView = Backbone.View.extend({
                 selectedModels.push(_this.collection.at(index).toJSON());
             }
         });
+        
+        this.clearMessage();
+        var errorMessage = this.doCallback(this.options.validate, [selectedModels]);
+        if(errorMessage) {
+            this.alertMessage(errorMessage);
+        } else {
+            this.doCallback(this.options.ok, [selectedModels]);
+        }
+    },
+    cancel: function() {
+        this.doCallback(this.options.cancel);
+    },
+    /**
+     * @param msg message to show on status bar
+     */
+    alertMessage: function(msg) {
+        this.$el.find('.is-status-bar')
+            .addClass('alert')
+            .append('<span class="label label-warning">Warning</span>')
+            .append('<span class="is-status-message is-text-overflow-ellipsis">' + msg + '</span>');
+    },
+    /**
+     * Clear message on status bar
+     */
+    clearMessage: function() {
+        this.$el.find('.is-status-bar')
+            .removeClass('alert')
+            .empty();
+    },
+    /**
+     * Overrite to custimize behavior
+     * @param selectedModels the models which is selected in item-selector
+     * @return error message; return null or nothing if validation is passed
+     */
+    validate: function(selectedModels) {
+        if(selectedModels.length < 1) {
+            return 'Please select at least 1 item.';
+        }
+    },
+    /**
+     * Overrite to custimize behavior
+     * @param selectedModels the models which is selected in item-selector
+     */
+    ok: function(selectedModels) {
         alert(JSON.stringify(selectedModels, '', 4));
     }
 });
@@ -130,7 +188,7 @@ ItemsView = Backbone.View.extend({
 /**
  * 1. Generate nested divs
  * 2. Calculate the width of inner div before and after scrollbar becomes visible
- * @returns calculated scrollbar width
+ * @return calculated scrollbar width
  */
 function getScrollbarWidth() {
     var $tempNode = 
